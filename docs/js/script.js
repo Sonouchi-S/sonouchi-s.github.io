5e7d60de10859201470f22ef32194a1e3116cc3d
Shu <shuhei.sonouchi@gmail.com>
2024-12-30 02:29:34 +0900

    Shu make repos.
    Add index.html ,css,img,js folders.

---
+++ docs/js/script.js
@@ -0,0 +1,15 @@
+function headerClick(area){
+    let offDisplayArea=document.querySelectorAll('div.onDisplay');
+    offDisplayArea.forEach(items=>{items.className="offDisplay";});
+    let targetArea=document.getElementById(area);
+    targetArea.className="onDisplay";
+}
+
+let smallImg=document.querySelectorAll('img.smallImg');
+let mainImg=document.getElementById('mainImg');
+smallImg.forEach(smallImg=>{
+    smallImg.addEventListener('mouseover',(targetimg)=>{
+        mainImg.src=targetimg.target.src;
+        mainImg.animate({opacity:[0,1]},500);
+    });
+});
